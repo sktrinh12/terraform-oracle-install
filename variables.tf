014448a54b1614acdc32913cf6214e9f52e619a3
spencer.trinh <spencer.trinh@kinnate.com>
2022-12-22 18:08:43 -0800

    change volume size, itype, add count

--- variables.tf
+++ variables.tf
@@ -1,16 +1,15 @@
 variable "awsprops" {
     type = map
     default = {
+    count = 2
     region = "us-west-2"
     ami = "ami-0bf3b3e2db4302789" // OL7.9-x86_64-HVM-2020-12-07
     vpc = "vpc-031ebba25c9df51eb"
-    itype = "t3.medium"
+    itype = "r4.large"
     subnet = "subnet-086ffcb03661d67a2"
     publicip = false
     keyname = "eks-apps" 
-    volume_size =  100
+    volume_size =  250
     volume_type = "gp3"
-    # csv_file = "security_files/aws_credentials.csv"
-    
   }
 }
